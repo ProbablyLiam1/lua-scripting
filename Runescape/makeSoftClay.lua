be917dc2622a801199ad0ffb201ca5ce0280270c
ProbablyLiam1 <145511761+ProbablyLiam1@users.noreply.github.com>
2023-09-19 19:05:20 -0500

    Create makeSoftClay.lua

---
+++ Runescape/makeSoftClay.lua
@@ -0,0 +1,51 @@
+--[[
+This script was setup to automate the process of creating soft clay in Old School RuneScape
+Intended Inventory Slots: First inventory slot should be a bucket, all other inventory slots should be clay (hard)
+Intended Location: The Fountain by Varrok East Bank, player should zoom in until fountain is overlapping their inventory to the left
+The below methods should be copy pasted into base.lua, and then makeSoftClay(27) should be pasted into the onEvent function to setup this script
+--]]
+
+--[[
+make soft clay at the varrok fountain
+requires a bucket/jug in your first inv slot, fill other slots with clay
+only works for full inv right now, howMuchClay can be changed in future
+]]--
+function makeSoftClay(howMuchClay)
+	--begin clay time
+	OutputLogMessage("ITS CLAY TIME")
+	howMuchClay = 5
+	for i = howMuchClay,1,-1
+	do
+		--left click the bucket
+		OutputLogMessage("Clicking Bucket\n")
+		clickInventorySlot(1,1,1)
+		Sleep(1500)
+		--left click the water source to the left of your inv
+		OutputLogMessage("Clicking Water\n")
+		clickWaterSource()
+		Sleep(2000)
+		--left click the bucket
+		OutputLogMessage("Clicking Bucket\n")
+		clickInventorySlot(1,1,1)
+		Sleep(1000)
+		--left click the hard clay in your last slot
+		OutputLogMessage("Clicking Clay\n")
+		clickInventorySlot(4,7,1)
+		Sleep(1000)
+		OutputLogMessage("Clay Left %d\n", i-1)
+	end
+	OutputLogMessage("CLAY TIME IS OVER")
+end
+
+--[[
+clicks just left of your inventory where a water source should be positioned
+]]--
+function clickWaterSource()
+	waterSourceX = firstColumnPos - distanceBetweenEachColumn*2
+	waterSourceY = firstRowPos
+	MoveMouseTo(waterSourceX,waterSourceY)
+	Sleep(500)
+	PressMouseButton(1)
+	ReleaseMouseButton(1)
+	--OutputLogMessage("Clicked Water Source at: %d,%d\n",waterSourceX,waterSourceY)
+end
